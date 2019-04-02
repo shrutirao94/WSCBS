b52eddc4438958bb0ddb81aeb3d2671b512bc05d
Shruti Rao <shrutirao94@gmail.com>
2019-04-02 22:08:46 +0200

    SOAP server

--- assignment-1/server.py
+++ assignment-1/server.py
@@ -1,3 +1,22 @@
-from zeep import Client
+from flask import Flask
+from flask_spyne import Spyne
+from spyne.protocol.soap import Soap11
+from spyne.model.primitive import Unicode, Integer
+from spyne.model.complex import Iterable
 
-client = Client('http://localhost:8080/')
+app = Flask(__name__)
+spyne = Spyne(app)
+
+class CalculatorSoapService(spyne.Service):
+    __service_url_path__ = './calculatorservice'
+    __in_protocol__ = Soap11(validator='lxml')
+    __out_protocol__ = Soap11()
+
+    @sypne.srpc(Unicode, Integer, _returns=Iterable(Unicode))
+    def echo(str, cnt):
+        for i in range(cnt):
+            yield str
+
+
+if __name__ == '__main__':
+    app.run(host = '127.0.0.1')
