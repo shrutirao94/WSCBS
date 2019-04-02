3ed655a425056f3e9e5b916674440daf6c008b4e
Shruti Rao <shrutirao94@gmail.com>
2019-04-02 14:23:32 +0200

    Simple Calculator App

---
+++ calculator.py
@@ -0,0 +1,20 @@
+class Calculator:
+    def __init__(self, input1,  input2)
+    self.input1 = input1
+    self.input2 = input2
+
+    def add(x, y):
+        return x.input1 + y.input2
+
+    def sub(x, y):
+        return x.input1 - y.input2
+
+    def mul(x, y):
+        return x.input1 * y.input2
+
+    def div(x, y):
+        return x.input1  / y.input2
+
+
+   calc_sum = Calculator(1, 2)
+   calc_sum.add()
