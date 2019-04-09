098e81f1e842c1a3cf4aea8cc6c4b7c42e81c692
Shruti Rao <shrutirao94@gmail.com>
2019-04-09 16:39:15 +0200

    Converter from int to string and string to int

--- assignment-1/url-shortner/url-shortner.py
+++ assignment-1/url-shortner/url-shortner.py
@@ -4,7 +4,7 @@ import string
 def toBase62(num, b = 62):
     if b <= 0 or b > 62:
         return 0
-    base = string.digits + string.lowercase + strimg.uppercase
+    base = string.digits + string.ascii_lowercase + string.ascii_uppercase
     r = num % b
     res = base[r]
     quotient = floor(num/b)
@@ -14,3 +14,17 @@ def toBase62(num, b = 62):
         quotient = floor(quotient / b)
         res = base[int(r)] + res
     return res
+
+def tobase10(num, b = 62):
+    base = string.digits + string.ascii_lowercase + string.ascii_uppercase
+    limit = len(num)
+    result = 0
+
+    for i in range(limit):
+        result = b * result + base.find(num[i])
+
+    return result
+
+encoder = toBase62(1234)
+print(toBase62(1234))
+print(tobase10(encoder))
